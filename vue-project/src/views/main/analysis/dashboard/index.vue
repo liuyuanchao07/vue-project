2e11520588282cd4bd46dfd4d93a886144c6de18
liuyuanchao07 <46569278+liuyuanchao07@users.noreply.github.com>
2025-02-28 15:54:57 +0800

    echarts

--- vue-project/src/views/main/analysis/dashboard/index.vue
+++ vue-project/src/views/main/analysis/dashboard/index.vue
@@ -9,33 +9,23 @@
     </div>
     <div class="dashboard_echart">
       <el-row :gutter="12">
-        <el-col :span="7"
-          ><el-card>
-            <template #header>
-              <div class="card-header">
-                <span>Card name</span>
-              </div>
-            </template>
-          </el-card></el-col
-        >
+        <el-col :span="7">
+          <PieCard :option="optionPie">
+            <template #header>饼状图</template>
+          </PieCard>
+        </el-col>
         <el-col :span="10"
           ><el-card>
             <template #header>
-              <div class="card-header">
-                <span>Card name</span>
-              </div>
+              <div class="card-header">卡片的头部标题</div>
             </template>
           </el-card></el-col
         >
-        <el-col :span="7"
-          ><el-card>
-            <template #header>
-              <div class="card-header">
-                <span>Card name</span>
-              </div>
-            </template>
-          </el-card></el-col
-        >
+        <el-col :span="7">
+          <PieCard :option="optionRose">
+            <template #header>玫瑰图</template>
+          </PieCard>
+        </el-col>
       </el-row>
     </div>
   </div>
@@ -44,8 +34,73 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue"
 import { getStatisticData } from "@/service/request"
+import type { EChartsOption } from "echart"
 import StatisticCard from "@/views/main/analysis/dashboard/statisticCard.vue"
+import PieCard from "@/views/main/analysis/dashboard/pieCard.vue"
 const statisticData = ref([])
+
+const optionPie: EChartsOption = {
+  tooltip: {
+    trigger: "item",
+  },
+  legend: {
+    orient: "horizontal",
+    left: "left",
+    itemWidth: 20,
+    itemHeight: 10,
+  },
+  series: [
+    {
+      name: "Access From",
+      type: "pie",
+      radius: "60%",
+      data: [
+        { value: 1048, name: "衣服" },
+        { value: 735, name: "裤子" },
+        { value: 580, name: "鞋子" },
+        { value: 484, name: "家具" },
+        { value: 300, name: "床品" },
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: "rgba(0, 0, 0, 0.5)",
+        },
+      },
+    },
+  ],
+}
+
+const optionRose: EChartsOption = {
+  legend: {
+    top: "bottom",
+  },
+  legend: {
+    orient: "horizontal",
+    left: "left",
+    itemWidth: 20,
+    itemHeight: 10,
+  },
+  series: [
+    {
+      name: "Nightingale Chart",
+      type: "pie",
+      radius: "55%",
+      roseType: "area",
+      itemStyle: {
+        borderRadius: 8,
+      },
+      data: [
+        { value: 40, name: "衣服" },
+        { value: 38, name: "裤子" },
+        { value: 32, name: "鞋子" },
+        { value: 30, name: "家具" },
+        { value: 28, name: "床品" },
+      ],
+    },
+  ],
+}
 
 onMounted(() => {
   const loading = ElLoading.service({
@@ -63,5 +118,9 @@ onMounted(() => {
 <style lang="less" scoped>
 .dashboard_echart {
   margin-top: 20px;
+}
+
+.card-header {
+  font-size: 18px;
 }
 </style>
---
+++ vue-project/src/views/main/analysis/dashboard/pieCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-card>
+    <template #header>
+      <div class="card-header">
+        <slot name="header"></slot>
+      </div>
+    </template>
+    <div class="pie_content" style="width: 100%; height: 300px" ref="chartRef"></div>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, defineProps } from "vue"
+import * as echarts from "echarts"
+const chartRef = ref(null)
+const props = defineProps(["option"])
+
+onMounted(() => {
+  const myChart = echarts.init(chartRef.value)
+  myChart.setOption(props.option)
+})
+</script>
+
+<style lang="less" scoped>
+.card-header {
+  font-size: 18px;
+}
+</style>
